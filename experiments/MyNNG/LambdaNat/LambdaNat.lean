bc03ddce9a0493c5cebe07b0a64c7942ac5acdff
Lean 4 VS Code Extension <>
2026-02-15 10:25:03 +0000

    Initial commit

---
+++ experiments/MyNNG/LambdaNat/LambdaNat.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `LambdaNat` library.
+-- Import modules here that should be built as part of the library.
+import LambdaNat.Basic
---
+++ experiments/MyNNG/LambdaNat/Main.lean
@@ -0,0 +1,4 @@
+import LambdaNat
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
